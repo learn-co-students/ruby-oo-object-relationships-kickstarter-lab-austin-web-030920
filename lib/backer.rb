ea4fb0a0461c6a4124b945e25112c439eaa53851
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-03-13 11:53:52 -0500

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+
+
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        ProjectBacker.all.select{|p| p.backer == self}.map{|pb| pb.project}
+    end
+
+
+
+end
